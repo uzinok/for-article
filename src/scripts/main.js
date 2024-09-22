5e4d401ea3a71774d82e1b5100d45c6bcc8fefce
uzinok <johndoe@example.com>
2024-09-23 02:41:32 +0300

    1

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,6 @@
 const customSelect = document.querySelector('.custom-select');
 const button = customSelect.querySelector('.custom-select__button');
+const buttonText = button.querySelector('span');
 const list = customSelect.querySelector('.custom-select__list');
 const input = customSelect.querySelector('.custom-select__input');
 const itemList = customSelect.querySelectorAll('.custom-select__item');
@@ -19,14 +20,61 @@ const updateItemView = (elem) => {
 }
 
 const updateButtonText = (text) => {
-	button.innerText = text;
+	buttonText.innerText = text;
 }
 
-const onListClick = (evt) => {
+const onClickList = (evt) => {
 	if (evt.target.dataset.value) {
 		const target = evt.target;
-		const value = evt.target.dataset.value;
-		const text = evt.target.innerText;
+		const value = target.dataset.value;
+		const text = target.innerText;
+
+		updateInputValue(value);
+		updateItemView(target);
+		updateButtonText(text);
+		closeCustomSelect();
+	}
+}
+
+const closeCustomSelect = () => {
+	customSelect.classList.remove('custom-select--view');
+}
+
+const nextValue = () => {
+	let target;
+
+	if (!customSelect.querySelector('.custom-select__item--active')) {
+		target = itemList[0];
+	} else {
+		itemList.forEach((item, index) => {
+			if (!target) {
+				target = item.classList.contains('custom-select__item--active') ? itemList[index + 1 >= itemList.length ? itemList.length - 1 : index + 1] : '';
+			}
+		});
+	}
+
+	const value = target.dataset.value;
+	const text = target.innerText;
+
+	updateInputValue(value);
+	updateItemView(target);
+	updateButtonText(text);
+}
+
+const prewValue = () => {
+	let target;
+
+	if (customSelect.querySelector('.custom-select__item--active')) {
+		itemList.forEach((item, index) => {
+			console.log(index - 1 === 0 ? 0 : index - 1);
+			if (!target) {
+
+				target = item.classList.contains('custom-select__item--active') ? itemList[index - 1 <= 0 ? 0 : index - 1] : '';
+			}
+		});
+
+		const value = target.dataset.value;
+		const text = target.innerText;
 
 		updateInputValue(value);
 		updateItemView(target);
@@ -34,6 +82,26 @@ const onListClick = (evt) => {
 	}
 }
 
+const onkeydownCustomSelect = (evt) => {
+	console.log(evt.key);
+
+	if (evt.key === 'ArrowUp') {
+		evt.preventDefault();
+		prewValue();
+	}
+
+	if (evt.key === 'ArrowDown') {
+		evt.preventDefault();
+		nextValue();
+	}
+
+	if (evt.key === 'Escape') {
+		closeCustomSelect();
+	}
+}
+
 button.addEventListener('click', onClickButton);
 
-list.addEventListener('click', onListClick);
+list.addEventListener('click', onClickList);
+
+customSelect.addEventListener('keydown', onkeydownCustomSelect)
