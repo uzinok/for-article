6c385107d08eded7054dc65cbb830650260bde24
uzinok <41639106+uzinok@users.noreply.github.com>
2024-09-23 00:49:40 +0300

    1

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,10 +1,39 @@
 const customSelect = document.querySelector('.custom-select');
-const button = document.querySelector('.custom-select__button');
+const button = customSelect.querySelector('.custom-select__button');
+const list = customSelect.querySelector('.custom-select__list');
+const input = customSelect.querySelector('.custom-select__input');
+const itemList = customSelect.querySelectorAll('.custom-select__item');
 
 const onClickButton = () => {
 	customSelect.classList.toggle('custom-select--view');
 }
 
+const updateInputValue = (value) => {
+	input.value = value;
+}
+
+const updateItemView = (elem) => {
+	itemList.forEach((item) => {
+		item.classList.toggle('custom-select__item--active', item.dataset.value === elem.dataset.value);
+	});
+}
+
+const updateButtonText = (text) => {
+	button.innerText = text;
+}
+
+const onListClick = (evt) => {
+	if (evt.target.dataset.value) {
+		const target = evt.target;
+		const value = evt.target.dataset.value;
+		const text = evt.target.innerText;
+
+		updateInputValue(value);
+		updateItemView(target);
+		updateButtonText(text);
+	}
+}
+
 button.addEventListener('click', onClickButton);
 
-console.log(customSelect);
+list.addEventListener('click', onListClick);
