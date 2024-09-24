c265c25ae741acd9e1c963f957cebb0af8433bf9
uzinok <johndoe@example.com>
2024-09-25 02:29:18 +0300

    1

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,7 +6,12 @@ const input = customSelect.querySelector('.custom-select__input');
 const itemList = customSelect.querySelectorAll('.custom-select__item');
 
 const onClickButton = () => {
-	customSelect.classList.toggle('custom-select--view');
+	if (customSelect.classList.contains('custom-select--view')) {
+		closeCustomSelect();
+		return;
+	}
+
+	openCustomSelect();
 }
 
 const updateInputValue = (value) => {
@@ -36,71 +41,16 @@ const onClickList = (evt) => {
 	}
 }
 
+const openCustomSelect = () => {
+	customSelect.classList.add('custom-select--view');
+
+	list.addEventListener('click', onClickList);
+}
+
 const closeCustomSelect = () => {
 	customSelect.classList.remove('custom-select--view');
-}
 
-const nextValue = () => {
-	let target;
-
-	if (!customSelect.querySelector('.custom-select__item--active')) {
-		target = itemList[0];
-	} else {
-		itemList.forEach((item, index) => {
-			if (!target) {
-				target = item.classList.contains('custom-select__item--active') ? itemList[index + 1 >= itemList.length ? itemList.length - 1 : index + 1] : '';
-			}
-		});
-	}
-
-	const value = target.dataset.value;
-	const text = target.innerText;
-
-	updateInputValue(value);
-	updateItemView(target);
-	updateButtonText(text);
-}
-
-const prewValue = () => {
-	let target;
-
-	if (customSelect.querySelector('.custom-select__item--active')) {
-		itemList.forEach((item, index) => {
-			if (!target) {
-
-				target = item.classList.contains('custom-select__item--active') ? itemList[index - 1 <= 0 ? 0 : index - 1] : '';
-			}
-		});
-
-		const value = target.dataset.value;
-		const text = target.innerText;
-
-		updateInputValue(value);
-		updateItemView(target);
-		updateButtonText(text);
-	}
-}
-
-const onkeydownCustomSelect = (evt) => {
-	if (evt.key === 'ArrowUp' || evt.key === 'ArrowLeft') {
-		console.log('up || left');
-
-		evt.preventDefault();
-		prewValue();
-	}
-
-	if (evt.key === 'ArrowDown' || evt.key === 'ArrowRight') {
-		evt.preventDefault();
-		nextValue();
-	}
-
-	if (evt.key === 'Escape') {
-		closeCustomSelect();
-	}
+	list.removeEventListener('click', onClickList);
 }
 
 button.addEventListener('click', onClickButton);
-
-list.addEventListener('click', onClickList);
-
-customSelect.addEventListener('keydown', onkeydownCustomSelect)
