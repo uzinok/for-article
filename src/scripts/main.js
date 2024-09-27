c1f4c2514f6bc4825d6ef4bf728abb353fdc80e8
uzinok <johndoe@example.com>
2024-09-27 03:05:19 +0300

    1

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,8 +1,10 @@
 const textCircle = document.querySelector('.text-circle');
 const newTextArray = [];
 
+textCircle.setAttribute('style', `--total: ${textCircle.innerText.length};`);
+
 textCircle.innerText.split('').forEach((element, i) => {
-	newTextArray.push(`<span class="char-${i}">${element}</span>`);
+	newTextArray.push(`<span style="--index: ${i}">${element}</span>`);
 });
 
 textCircle.innerHTML = newTextArray.join('');
