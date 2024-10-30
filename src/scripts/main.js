7fb6d53190fa563f6f5fbd99295d7b9f1ac9c58d
uzinok <johndoe@example.com>
2024-10-30 12:48:57 +0300

    1

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,7 +2,5 @@ const slider = document.querySelector('.slider');
 const range = document.querySelector('.slider__range-js');
 
 range.addEventListener('input', () => {
-	console.log(range.value);
-
-	slider.style.setProperty('--value', range.value + '%');
+  slider.style.setProperty('--value', range.value + '%');
 });
