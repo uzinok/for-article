334a80fd6d78404c0e79e9b53ba0c41d9acab3d2
uzinok <johndoe@example.com>
2024-11-03 03:16:36 +0300

    1

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,6 +1,19 @@
-const slider = document.querySelector('.slider');
-const range = document.querySelector('.slider__range-js');
+const rangeWrap = document.querySelector('.range-wrap');
+const range = document.querySelector('.range');
+const output = document.querySelector('.output');
 
-range.addEventListener('input', () => {
-  slider.style.setProperty('--value', range.value + '%');
-});
+rangeWrap.classList.remove('no-js');
+
+const onRangeInput = () => {
+	const value = range.value;
+	const min = range.min;
+	const max = range.max;
+
+	const valuePercent = `${100 - ((max - value) / (max - min) * 100)}%`;
+
+	range.style.backgroundSize = `${valuePercent} 100%`;
+	output.value = value;
+}
+
+onRangeInput();
+range.addEventListener('input', onRangeInput);
