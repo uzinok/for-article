80cc5a063eae9a4ee8fe25878fae8f8d8b5d1131
uzinok <41639106+uzinok@users.noreply.github.com>
2024-11-09 15:10:26 +0300

    1

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,28 +1,13 @@
-const rangeWrap = document.querySelector('.range-wrap');
+const rangeWrap = document.querySelector('.range-wrap'); // получаем родительский блок output
+
+rangeWrap.classList.remove('no-js'); // удаляем класс no-js
+
 const range = document.querySelector('.range');
 const output = document.querySelector('.output');
 
-rangeWrap.classList.remove('no-js');
-
 const onRangeInput = () => {
 	const value = range.value;
-	const min = range.min;
-	const max = range.max;
-
-	const valuePercent = `${100 - ((max - value) / (max - min) * 100)}%`;
-
-	range.style.backgroundSize = `${valuePercent} 100%`;
 	output.value = value;
 }
-
 onRangeInput();
 range.addEventListener('input', onRangeInput);
-
-
-
-// const slider = document.querySelector('.slider');
-// const range = document.querySelector('.slider__range-js');
-
-range.addEventListener('input', () => {
-	rangeWrap.style.setProperty('--value', range.value + '%');
-});
