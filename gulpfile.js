82c169f9092c9912981f00605bfcd8ec75ea4dea
uzinok <41639106+uzinok@users.noreply.github.com>
2024-11-26 16:55:03 +0300

    1

--- gulpfile.js
+++ gulpfile.js
@@ -79,7 +79,7 @@ export function copy() {
 			"./src/fonts/*.{woff2,woff}",
 			"./src/*.ico",
 			"./src/img/**/*.{svg,jpg,jpeg,png,webp,avif}",
-			"./src/video/**/*.{mp4,webm,vtt}",
+			"./src/video/**/*.{mp4,webm}",
 			"./src/static/**/*.{css,js}",
 		], {
 			base: paths.src
