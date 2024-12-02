448ebba45f494fdb0ab41e518ff2db1467bb1aa5
uzinok <41639106+uzinok@users.noreply.github.com>
2024-12-02 18:39:15 +0300

    1

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,58 +1,27 @@
 const customVideo = document.querySelector('.custom-video');
 const video = customVideo.querySelector('.custom-video__elements');
 const buttonStart = customVideo.querySelector('.custom-video__start');
-const playPause = customVideo.querySelector('.custom-video__play-pause');
+const buttonPlayPause = customVideo.querySelector('.custom-video__play-pause');
 const timeRange = customVideo.querySelector('.custom-video__range-elements');
 const timeBefore = customVideo.querySelector('.custom-video__range-before');
 const timeAfter = customVideo.querySelector('.custom-video__range-after');
 const fullscreen = customVideo.querySelector('.custom-video__fullscreen');
-const soundButton = customVideo.querySelector('.custom-video__sound-button');
+const buttonSound = customVideo.querySelector('.custom-video__sound-button');
 const soundRange = customVideo.querySelector('.custom-video__sound-range');
-
-// добавить проверку: видео закончилось addEventListener ended
-// если по каким то причинам отсталось примерно 10ms то по клику плей - начать воспроизведение с начала
-// продумать изменение уровня звука на мобилке.
 
 // удаляем нативные элементы управления
 video.controls = false;
 
-if (video.muted && video.autoplay) {
-	// если автовоспроизведение включено и отключен звук
-	// показываем кнопку старт
-	// кнопке play/pause задаём класс custom-video__play-pause--played
-	// указываем что видео играет
-	buttonStart.style.display = 'block';
-	playPause.classList.add('custom-video__play-pause--played');
-} else {
-	// кнопку можно и не удалять, но слушатель этой кнопки, который ниже, необходимо удалить
-	buttonStart.remove();
+// all
+const toggleMutted = () => {
+	// визуально переключаем внешний вид чтобы было видно когда звук отключен
+	// если звук включается, то и рейнджу указываем уровень звука видео
+	// добавить проверку после включения включения звука: если уровень громкости 0, то поставить 0.1
+	buttonSound.classList.toggle('custom-video__sound-button--muted');
+	video.muted = !video.muted;
+
+	soundRange.value = video.muted ? 0 : video.volume;
 }
-
-buttonStart.addEventListener('click', () => {
-	// если видна кнопка start, отслеживаем клик
-	// удаляем кнопку, чтобы не мешалась и чтобы удалить лишние слушатели
-	// включаем звук и перематываем на начало видео
-	buttonStart.remove();
-	video.muted = false;
-});
-
-playPause.addEventListener("click", (e) => {
-	// по клику на кнопку play/pause
-	if (video.paused || video.ended) {
-		// если видео на паузе или достигло конца
-		// включаем воспроизведение видео
-		// задаём кноке класс custom-video__play-pause--played
-		if (video.duration - video.currentTime <= 10) {
-			video.currentTime = 0;
-		}
-		video.play();
-		playPause.classList.add('custom-video__play-pause--played');
-
-	} else {
-		video.pause();
-		playPause.classList.remove('custom-video__play-pause--played');
-	}
-});
 
 const msToTime = (duration) => {
 	// получаем время в миллисекундах, переводим в понятное время в формате чч:мм:сс
@@ -65,16 +34,53 @@ const msToTime = (duration) => {
 	return hours + ":" + minutes + ":" + seconds;
 }
 
-video.addEventListener("loadedmetadata", () => {
+const toggleButtonPlayPause = () => {
+	buttonPlayPause.classList.toggle('custom-video__play-pause--played');
+}
+
+const setFullscreenData = (state) => {
+	customVideo.setAttribute("data-fullscreen", !!state);
+}
+
+// handle
+const handleButtonStart = () => {
+	// если видна кнопка start, отслеживаем клик
+	// удаляем слушатель этой кнопки
+	// включаем звук и перематываем на начало видео
+	buttonStart.removeEventListener('click', handleButtonStart);
+	buttonStart.classList.add('hide')
+	toggleMutted();
+}
+
+const handleButtonPlayPause = () => {
+	// по клику на кнопку play/pause
+	if (video.paused || video.ended) {
+		// если достигло конца, ставим на начало
+		// включаем воспроизведение видео
+		// задаём кнопке класс custom-video__play-pause--played
+		if (video.duration - video.currentTime <= 10) {
+			video.currentTime = 0;
+		}
+		video.play();
+		toggleButtonPlayPause();
+	} else {
+		// ставим видео на паузу
+		// меняем внешний вид кнопки play/Pause
+		video.pause();
+		buttonPlayPause.classList.remove('custom-video__play-pause--played');
+	}
+}
+
+const handleLoadedMetaData = () => {
 	// когда загнрузятся данные о видео
-	// указываем рейнджу максимальное значение
+	// указываем рейнджу максимальное значение (обязательно атрибутом)
 	// указываем пользователю длительноесть видео
 	timeRange.setAttribute("max", video.duration);
 	timeAfter.innerText = msToTime(0);
 	timeBefore.innerText = msToTime(video.duration * 1000);
-});
+}
 
-video.addEventListener("timeupdate", () => {
+const handleTimeUpdate = () => {
 	// когда видео воспроизводится
 	// проверяем, наличие атрибута макс.
 	// если нет, то добавляем этот атрибут и общую длительность видео
@@ -86,50 +92,42 @@ video.addEventListener("timeupdate", () => {
 
 	timeRange.value = video.currentTime;
 	timeAfter.innerText = msToTime(video.currentTime * 1000);
-});
+}
 
-timeRange.addEventListener('input', () => {
+const handleEnded = () => {
+	buttonPlayPause.classList.remove('custom-video__play-pause--played');
+}
+
+const handleTimeRange = () => {
 	// если пользователь меняет значение рейнджа
 	// меняем время и у видео
 
 	video.currentTime = +timeRange.value;
-});
-
-video.addEventListener('ended', () => {
-	playPause.classList.remove('custom-video__play-pause--played');
-});
-
-if (video.muted) {
-	// если есть атрибут muted у видео
-	// кнопке звука задаем класс, чтобы сообщить пользователю о выключенном звуке
-	// рейнджу звука устанавливаем 0
-	soundButton.classList.add('custom-video__sound-button--muted');
-	soundRange.value = 0;
 }
 
-soundButton.addEventListener('click', () => {
+const handleButtonSound = () => {
 	// по клику на кнопку выключения звкука,
 	// визуально переключаем внешний вид чтобы было видно когда звук отключен
-	// если звук включается, то и рейнджу указываем уровень звука видео
-	// добавить проверку после включения включения звука: если уровень громкости 0, то поставить 0.1
-	soundButton.classList.toggle('custom-video__sound-button--muted');
+	// включаем/выключаем звук
+	if (video.volume === 0) {
+		return;
+	}
+
+	buttonSound.classList.toggle('custom-video__sound-button--muted');
 	video.muted = !video.muted;
+}
 
-	soundRange.value = video.muted ? 0 : video.volume;
-});
-
-soundRange.addEventListener('input', () => {
+const handleSoundRange = () => {
 	// если пользователь меняет значение уровня громкости
 	// меняем уровень громкости у видео
 	// у кнопки включения/выключения звука, добавляем соответствующие классы для визуализации
+	// при нулевой громкости указываем что у видео отключен звук
 	video.volume = soundRange.value;
 
-	soundButton.classList.toggle('custom-video__sound-button--down', +soundRange.value <= 0.5);
-	soundButton.classList.toggle('custom-video__sound-button--muted', +soundRange.value === 0);
-});
+	buttonSound.classList.toggle('custom-video__sound-button--down', +soundRange.value <= 0.5);
+	buttonSound.classList.toggle('custom-video__sound-button--muted', +soundRange.value === 0);
 
-const setFullscreenData = (state) => {
-	customVideo.setAttribute("data-fullscreen", !!state);
+	video.muted = !video.volume;
 }
 
 const handleFullscreen = () => {
@@ -147,5 +145,41 @@ const handleFullscreen = () => {
 		// добавить проверку, если пользовать какое-то время не двигает мышкой - скрыть контроллер, при возобновлении движении мышки - опять показать
 	}
 };
+
+// init
+if (video.muted && video.autoplay) {
+	// если автовоспроизведение включено и отключен звук
+	// показываем кнопку старт
+	// кнопке play/pause задаём класс custom-video__play-pause--played, указываем что видео играет
+	// показывем что отключен звук у кнопки buttonSound
+	buttonStart.classList.remove('hide');
+	toggleButtonPlayPause();
+	buttonSound.classList.add('custom-video__sound-button--muted');
+} else {
+	// или просто удаляем слушатель этой кнопки
+	buttonStart.removeEventListener('click', handleButtonStart);
+}
+
+if (video.muted) {
+	// если есть атрибут muted у видео
+	// кнопке звука задаем класс, чтобы сообщить пользователю о выключенном звуке
+	// рейнджу звука устанавливаем 0
+	buttonSound.classList.add('custom-video__sound-button--muted');
+	soundRange.value = 0;
+}
+
+video.addEventListener("loadedmetadata", handleLoadedMetaData);
+
+// work
+buttonStart.addEventListener('click', handleButtonStart);
+
+buttonPlayPause.addEventListener('click', handleButtonPlayPause);
+
+video.addEventListener("timeupdate", handleTimeUpdate);
+video.addEventListener('ended', handleEnded);
+timeRange.addEventListener('input', handleTimeRange);
+
+buttonSound.addEventListener('click', handleButtonSound);
+soundRange.addEventListener('input', handleSoundRange);
 
 fullscreen.addEventListener('click', handleFullscreen);
