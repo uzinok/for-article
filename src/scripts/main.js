e9cca0ae29fac12987e5adc7e7e31dd7162dfc1f
uzinok <johndoe@example.com>
2025-01-23 02:15:56 +0300

    для новой темы

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,32 +1,21 @@
-// const input = document.querySelector('.password-input');
-// const button = document.querySelector('.password-button');
-// const buttonText = document.querySelector('.password-button__text');
+const container = document.querySelector('.textarea-container');
+const textareaVisible = container.querySelector('.textarea-visible');
 
-// const togglePassword = () => {
-// 	if (button.classList.contains('password-button--view')) {
-// 		input.type = 'password';
-// 		button.classList.remove('password-button--view');
-// 		buttonText.innerText = 'Показать';
-// 	} else {
-// 		input.type = 'text';
-// 		button.classList.add('password-button--view');
-// 		buttonText.innerText = 'Скрыть';
-// 	}
+const textareaHidde = document.createElement('div');
+textareaHidde.classList.add('textarea-hidde');
+container.append(textareaHidde);
 
-// 	input.focus();
-// }
+const onInputTextarea = () => {
+	textareaHidde.innerText = textareaVisible.value;
 
-// button.addEventListener('click', togglePassword);
+	if (textareaVisible.value) {
+		console.log(textareaHidde.offsetHeight + 'px');
 
-const input = document.querySelector('.password-input');
-const button = document.querySelector('.password-button');
-const buttonText = document.querySelector('.password-button__text');
-
-const togglePassword = () => {
-	const isViewMode = button.classList.toggle('password-button--view');
-	input.type = isViewMode ? 'text' : 'password';
-	buttonText.textContent = isViewMode ? 'Скрыть' : 'Показать';
-	input.focus();
+		textareaVisible.style.height = textareaHidde.offsetHeight + 'px';
+	}
 }
 
-button.addEventListener('click', togglePassword);
+textareaVisible.addEventListener('input', onInputTextarea);
+
+console.log(container);
+console.log(textareaVisible);
