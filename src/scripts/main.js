c964e68c6bcfcc0928c6cb2a9e845c3f56e2d74e
uzinok <41639106+uzinok@users.noreply.github.com>
2025-10-01 19:34:37 +0300

    1

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,6 +2,8 @@ const button = document.querySelector(".nav__toggle");
 const nav = document.querySelector(".nav");
 const navList = document.querySelector(".nav__list");
 
+nav.classList.remove("nav--no-js");
+
 function toggleNav() {
 	const isExpanded = nav.classList.toggle("nav--open");
 
